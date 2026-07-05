65d9a164a4bbf7d3063ce27d674eceb3a7cc78d9
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-07-05 15:30:36 +0600

    Refactor: Use CustomTextField and CustomButton in SecuritySettingsScreen

--- App Developer/catellio25/lib/features/profile/views/security_settings_screen.dart
+++ App Developer/catellio25/lib/features/profile/views/security_settings_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-
 import '../../../../core/theme/app_colors.dart';
 import '../../../../global_widgets/custom_text/custom_text.dart';
+import '../../../../global_widgets/custom_text_field/custom_text_field.dart';
+import '../../../../global_widgets/custom_button/custom_button.dart';
 import '../../../../global_widgets/custom_royel_appbar/custom_royel_appbar.dart';
 import '../view_models/profile_controller.dart';
 
@@ -41,23 +42,61 @@ class SecuritySettingsScreen extends GetView<ProfileController> {
                 children: [
                   CustomText(
                     text: "Update Password",
-                    fontSize: 20.sp,
+                    fontSize: 24.sp,
                     fontWeight: FontWeight.bold,
                     color: AppColors.black,
                   ),
                   SizedBox(height: 8.h),
                   CustomText(
                     text: "Ensure your account stays secure with a complex, unique password.",
-                    fontSize: 14.sp,
+                    fontSize: 16.sp,
                     color: AppColors.greyShade,
+                    textAlign: TextAlign.start,
+                    maxLines: 3,
 
                   ),
                   SizedBox(height: 24.h),
                   
-                  _buildPasswordField("Current Password", controller.isCurrentPasswordVisible, controller.toggleCurrentPassword),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        text: "Current Password",
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.greyShade,
+                      ),
+                      SizedBox(height: 8.h),
+                      CustomTextField(
+                        hintText: "••••••••",
+                        isPassword: true,
+                        fillColor: AppColors.white,
+                        fieldBorderColor: const Color(0xFFF0F0F0),
+                        fieldBorderRadius: 12.r,
+                      ),
+                    ],
+                  ),
                   SizedBox(height: 20.h),
                   
-                  _buildPasswordField("New Password", controller.isNewPasswordVisible, controller.toggleNewPassword),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        text: "New Password",
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.greyShade,
+                      ),
+                      SizedBox(height: 8.h),
+                      CustomTextField(
+                        hintText: "••••••••",
+                        isPassword: true,
+                        fillColor: AppColors.white,
+                        fieldBorderColor: const Color(0xFFF0F0F0),
+                        fieldBorderRadius: 12.r,
+                      ),
+                    ],
+                  ),
                   SizedBox(height: 12.h),
                   
                   // Password strength indicator
@@ -81,33 +120,39 @@ class SecuritySettingsScreen extends GetView<ProfileController> {
                   ),
                   SizedBox(height: 20.h),
                   
-                  _buildPasswordField("Confirm New Password", controller.isConfirmPasswordVisible, controller.toggleConfirmPassword),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CustomText(
+                        text: "Confirm New Password",
+                        fontSize: 12.sp,
+                        fontWeight: FontWeight.bold,
+                        color: AppColors.greyShade,
+                      ),
+                      SizedBox(height: 8.h),
+                      CustomTextField(
+                        hintText: "••••••••",
+                        isPassword: true,
+                        fillColor: AppColors.white,
+                        fieldBorderColor: const Color(0xFFF0F0F0),
+                        fieldBorderRadius: 12.r,
+                      ),
+                    ],
+                  ),
                   
                   SizedBox(height: 32.h),
                   
-                  GestureDetector(
+                  CustomButton(
                     onTap: () => Get.back(),
-                    child: Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.symmetric(vertical: 16.h),
-                      decoration: BoxDecoration(
-                        color: AppColors.black,
-                        borderRadius: BorderRadius.circular(12.r),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CustomText(
-                            text: "Update Password",
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.bold,
-                            color: AppColors.white,
-                          ),
-                          SizedBox(width: 8.w),
-                          Icon(Icons.arrow_forward, color: AppColors.white, size: 16.sp),
-                        ],
-                      ),
-                    ),
+                    title: "Update Password",
+                    fontSize: 14.sp,
+                    fillColor: AppColors.black,
+                    textColor: AppColors.white,
+                    borderRadius: 12.r,
+                    suffixIcon: Icons.arrow_forward,
+                    suffixIconColor: AppColors.white,
+                    imageSize: 16.sp,
+                    height: 52.h,
                   ),
                 ],
               ),
@@ -137,46 +182,4 @@ class SecuritySettingsScreen extends GetView<ProfileController> {
     );
   }
 
-  Widget _buildPasswordField(String label, RxBool isVisible, VoidCallback onToggle) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CustomText(
-          text: label,
-          fontSize: 12.sp,
-          fontWeight: FontWeight.bold,
-          color: AppColors.greyShade,
-        ),
-        SizedBox(height: 8.h),
-        Obx(() => Container(
-          width: double.infinity,
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
-          decoration: BoxDecoration(
-            color: AppColors.white,
-            borderRadius: BorderRadius.circular(12.r),
-            border: Border.all(color: const Color(0xFFF0F0F0)),
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              CustomText(
-                text: isVisible.value ? "password123" : "••••••••",
-                fontSize: 14.sp,
-                color: AppColors.black,
-
-              ),
-              GestureDetector(
-                onTap: onToggle,
-                child: Icon(
-                  isVisible.value ? Icons.visibility_off_outlined : Icons.visibility_outlined,
-                  color: AppColors.greyShade,
-                  size: 20.sp,
-                ),
-              ),
-            ],
-          ),
-        )),
-      ],
-    );
-  }
 }
