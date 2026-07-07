1f765c0db318316246a86a09c61d4b60d2c2f20c
codeMAHDI <moshiurrahmanmehedi210@gmail.com>
2026-07-07 16:02:12 +0600

    Refactor: Delete unused budget_insights feature folder

--- App Developer/catellio25/lib/features/budget_insights/bindings/budget_insights_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import '../view_models/budget_insights_controller.dart';
-
-class BudgetInsightsBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<BudgetInsightsController>(() => BudgetInsightsController());
-  }
-}
